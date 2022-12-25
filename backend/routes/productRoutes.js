9b1be67559386fae609909aafc34a3b18a8e2361
hugo <hugo8ds@gmail.com>
2022-12-25 16:26:44 -0300

    Bringing Redux State Into HomeScreen - useDispatch & useSelector

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,9 @@ const router = express.Router()
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
 
+    // res.status(401)
+    // throw new Error('Not Authorized')
+
     res.json(products)
 }))
 
