19f4fa7bafc909bf10336d4b2a940f4f835ec7b3
hugo <hugo8ds@gmail.com>
2022-12-25 15:27:56 -0300

    Product List Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,9 +3,14 @@
 // import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { configureStore } from '@reduxjs/toolkit'
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = {
+    productList: productListReducer,
+}
 
 const initialState = {}
 
-const store = configureStore({ reducer: {}, middleware: (getDefaultMiddleware) => getDefaultMiddleware(), devTools: true, initialState: initialState })
+const store = configureStore({ reducer: reducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware(), devTools: true, initialState: initialState })
 
 export default store
