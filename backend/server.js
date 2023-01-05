a5e152d4643c6d4cea3da02f102ad2b6e0b46138
hugo-ds <hugodsantana@gmail.com>
2023-01-05 15:06:30 -0300

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -16,7 +16,10 @@ app.get('/', (req, res) => {
     res.send('API is running...!')
 })
 
+app.use(express.json())
+
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Error handling
 app.use(notFound)
