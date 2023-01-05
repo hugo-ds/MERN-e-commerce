259a2303767c844ee8a8359d87bcf4372f753770
hugo-ds <hugodsantana@gmail.com>
2023-01-05 15:41:57 -0300

    Generate a JSON Web Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Authenticate user and get token
 // @route   POST /api/users/login
@@ -9,7 +10,13 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email: email })
 
     if (user && (await user.matchPassword(password))) {
-        res.json({ _id: user._id, name: user.name, email: user.email, isAdmin: user.isAdmin, token: null })
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
     } else {
         res.status(401)
         throw new Error('Invalid email or password')
