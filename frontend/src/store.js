c7c07a9d93a2507f269fb32b7af57ad85eeb95b8
hugo-ds <hugodsantana@gmail.com>
2023-01-06 15:26:02 -0300

    User Login Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = {
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 }
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
-console.log(cartItemsFromStorage)
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const preloadedState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const store = configureStore({
