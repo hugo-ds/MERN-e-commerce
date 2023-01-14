17b12658f2122c56828c1a7f0f08c7bcc990b963
hugo-ds <hugodsantana@gmail.com>
2023-01-14 15:13:13 -0300

    Admin Screen Access Security

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listUsers } from '../actions/userActions'
+import { useNavigate } from 'react-router-dom'
 
 const RegisterScreen = () => {
     const dispatch = useDispatch()
@@ -12,9 +13,18 @@ const RegisterScreen = () => {
     const userList = useSelector((state) => state.userList)
     const { loading, error, users } = userList
 
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
+    const navigate = useNavigate()
+
     useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            navigate('/login')
+        }
+    }, [dispatch, navigate, userInfo])
 
     const delteHandler = (id) => {
         console.log()
