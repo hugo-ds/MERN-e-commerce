d5221b00277ff8fa9521f81c528ced17c49b19b8
hugo-ds <hugodsantana@gmail.com>
2023-01-15 11:27:17 -0300

    Get User By ID & Update User Endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
     updateUserProfile,
     getUsers,
     deleteUser,
+    getUserById,
+    updateUser,
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -14,6 +16,6 @@ const router = express.Router()
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
