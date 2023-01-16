d61034e37b3c414cdbca5967c001c2b1634810f6
hugo-ds <hugodsantana@gmail.com>
2023-01-16 10:44:17 -0300

    Edit Product Screen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,138 @@
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { listProductDetails } from '../actions/productActions'
+
+const ProductEditScreen = () => {
+    const { id: productId } = useParams()
+
+    const { loading, error, product } = useSelector((state) => state.productDetails)
+
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [image, setImage] = useState('')
+    const [brand, setBrand] = useState('')
+    const [category, setCategory] = useState('')
+    const [countInStock, setCountInStock] = useState(0)
+    const [description, setDescription] = useState('')
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!product.name || product._id !== productId) {
+            dispatch(listProductDetails(productId))
+        } else {
+            setName(product.name)
+            setPrice(product.price)
+            setImage(product.image)
+            setBrand(product.brand)
+            setCategory(product.category)
+            setCountInStock(product.countInStock)
+            setDescription(product.description)
+        }
+    }, [dispatch, navigate, productId, product])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        // dispatch(updateUser({ _id: userId, name, email, isAdmin }))
+    }
+
+    return (
+        <>
+            <Link to='/admin/productlist' className='btn btn-light my-3'>
+                Go back
+            </Link>
+            <FormContainer>
+                <h1>Edit Product</h1>
+                {loading ? (
+                    <Loader></Loader>
+                ) : error ? (
+                    <Message>{error}</Message>
+                ) : (
+                    <Form onSubmit={submitHandler}>
+                        <Form.Group controlId='name' className='py-3'>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type='name'
+                                placeholder='Enter name'
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='price' className='py-3'>
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type='bumber'
+                                placeholder='Enter price'
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='image' className='py-3'>
+                            <Form.Label>Image</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Enter image url'
+                                value={image}
+                                onChange={(e) => setImage(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='brand' className='py-3'>
+                            <Form.Label>Brand</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Enter brand'
+                                value={brand}
+                                onChange={(e) => setBrand(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='category' className='py-3'>
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Enter category'
+                                value={category}
+                                onChange={(e) => setCategory(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='countInStock' className='py-3'>
+                            <Form.Label>Count In Stock</Form.Label>
+                            <Form.Control
+                                type='bumber'
+                                placeholder='Enter count in stock'
+                                value={countInStock}
+                                onChange={(e) => setCountInStock(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='description' className='py-3'>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                type='text'
+                                placeholder='Enter description'
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+
+                        <Button type='submit' variant='primary'>
+                            Update
+                        </Button>
+                    </Form>
+                )}
+            </FormContainer>
+        </>
+    )
+}
+
+export default ProductEditScreen
