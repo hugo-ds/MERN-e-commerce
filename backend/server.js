126ede01033385095d2657808d2589fa5e9de694
hugo-ds <hugodsantana@gmail.com>
2023-01-19 11:36:44 -0300

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-    res.send('API is running...!')
-})
-
 app.use(express.json())
 
 // Routs
@@ -37,6 +33,15 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 // Make /uploads accessable.
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...!')
+    })
+}
 
 // Error handling
 app.use(notFound)
