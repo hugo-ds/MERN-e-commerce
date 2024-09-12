f027abd4208dede96c6683c507269742d17f5c70
hugo-ds <hugodsantana@gmail.com>
2024-09-12 20:58:49 -0300

    fix deprecated code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 
+mongoose.set('strictQuery', false)
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
